a5bd593b051a00e88860c0a3f9c92cf2cc7f8fea
asiduki <147263246+asiduki@users.noreply.github.com>
2024-09-25 00:51:33 +0530

    Work

--- Java/Calculator.java
+++ Java/Calculator.java
@@ -1,3 +1,21 @@
+import java.util.*;
+
 public class Calculator{
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Two Number");
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
+        System.out.println("Enter the operation ");
+        String a = sc.next() ;
+        switch (a) {
+            case "+" :
+                System.out.println("The sum of two number is " + number1 + number2);
+                break;
+        
+            default:
+            System.out.println("Select the correct opertaion");
+                break;
+        }
+    }
 }
